9584c17524c5b91832975677af68d368d17681a3
Jaelin Ford <jaelin.ford24@gmail.com>
2025-08-31 04:14:57 -0400

    Add paddle

--- main.lua
+++ main.lua
@@ -1,25 +1,29 @@
 BALL_X = 1
 BALL_Y = 40
-
 BALL_DX = 2
 BALL_DY = 2
-
 BALL_R = 2
+BALL_DR = 0.5
 
-FRAME = 0
+PAD_X = 52
+PAD_Y = 120
+PAD_W = 24
+PAD_H = 3
 
 function _init()
 	cls()
 end
 
 function _update()
-	FRAME = FRAME + 1
-	BALL_X = BALL_X + BALL_DX
-	BALL_Y = BALL_Y + BALL_DY
-
-	if BALL_R > 3 or BALL_R < 2 then
-		BALL_DR = -BALL_DR
+	if btn(0) then
+		PAD_X -= 5 -- Left
 	end
+	if btn(1) then
+		PAD_X += 5 -- Right
+	end
+
+	BALL_X += BALL_DX
+	BALL_Y += BALL_DY
 
 	if BALL_X > 127 or BALL_X < 0 then
 		BALL_DX = -BALL_DX
@@ -35,4 +39,5 @@ end
 function _draw()
 	rectfill(0, 0, 127, 127, 1)
 	circfill(BALL_X, BALL_Y, BALL_R, 10)
+	rectfill(PAD_X, PAD_Y, PAD_X + PAD_W, PAD_Y + PAD_H, 6)
 end
